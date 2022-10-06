cef274819a1c49a6c737658273df5d7705288d90
distvision <furede2@gmail.com>
2022-10-06 18:19:33 +0200

    Implementing routes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import { View, Image, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://8d02-185-253-99-226.ngrok.io/games')
+      .then(response => response.json())
+      .then(data => setGames(data)
+      )
+  }, [])
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -22,7 +32,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard
@@ -34,6 +44,6 @@ export function Home() {
         contentContainerStyle={styles.contentList}
       />
 
-    </View>
+    </SafeAreaView>
   );
 }
